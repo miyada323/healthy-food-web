dc65cc2900ebfeb0b271c338e392e63c52d8c961
miyada323 <miyadakhairy632@gmail.com>
2021-02-15 16:40:16 +0200

    healthy food web

---
+++ style/test.js
@@ -0,0 +1,17 @@
+function username() {
+
+    var email = document.getElementById("Enter your email").value ,
+
+    result = value
+
+    message = document.getElementById("message");
+
+if (username === "") {
+
+    message.innerHTML = "this filed can not be empty" ;
+
+} else {
+     
+    message.innerHTML = username + result ;
+}
+}
